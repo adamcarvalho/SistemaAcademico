5e3313fe37ee4d0a8a7bbb648c47f325db5f3002
Adam <Adam@LENOVO>
2018-10-07 18:43:19 -0300

    Criado os atributos da classe Aluno.

--- SistemaAcademico/src/Aluno.java
+++ SistemaAcademico/src/Aluno.java
@@ -1,4 +1,13 @@
 
 public class Aluno {
-
+	
+	String nome;
+	String cpf;
+	int matricula; 
+	String endereco;
+	String email;
+	String telefone;
+	String situacao;
+	String cursos;
+	String disciplinas;
 }
